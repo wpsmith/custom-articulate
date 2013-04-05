e56a59687e7210bd42c735c7df0602f67138e622
wpsmith <t@wpsmith.net>
2013-04-05 14:24:30 -0400

    Reorganized, added documentation, added Articulate functions

--- custom.js
+++ custom.js
@@ -1,3 +1,6 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, devel:false */
+/*global GetPlayer() */
+
 /**************
 Custom Articulate LMS Libary
 
@@ -7,28 +10,73 @@ Version 1.0.0
 // Instantiate Articulate Player
 var tsAPI = parent;
 var tsPlayer = '';
-var student = {};
 document.addEventListener( "DOMContentLoaded", function() {
-	tsPlayer = GetPlayer();
+	tsPlayer = tsGetPlayer();
 });
 
-// Correction to tsPlayer (if needed)
+/**
+ * Safe way to Articulate's GetPlayer().
+ */
 function tsGetPlayer() {
-	if ( 'undefined' === typeof tsPlayer )
+	"use strict";
+	
+	if ( 'function' === typeof GetPlayer ) {
 		tsPlayer = GetPlayer();
-	else if ( null === tsPlayer )
-		tsPlayer = GetPlayer();
+	}
+	else if ( 'function' === typeof window.scormdriver_content.GetPlayer ) {
+		tsPlayer = window.scormdriver_content.GetPlayer();
+	}
 	
 	return tsPlayer;
 }
 
+/**
+ * Gets Articulate Variable Value
+ */
+function tsGetVar( articulateVar ) {
+	"use strict";
+	tsPlayer.GetVar( articulateVar )
+}
+
+/**
+ * Sets Articulate Variable Value
+ */
+function tsSetVar( articulateVar, value ) {
+	"use strict";
+	tsPlayer.SetVar( articulateVar, value )
+}
+
+
+/*****
+UTILITY FUNCTIONS
+*****/
+
+/**
+ * Executes function from a string
+ *
+ * @link http://stackoverflow.com/questions/359788/how-to-execute-a-javascript-function-when-i-have-its-name-as-a-string
+ * Examples:
+ *   executeFunctionByName("My.Namespace.functionName", window, arguments);
+ *   executeFunctionByName("Namespace.functionName", My, arguments);
+ */
+function tsExecuteFunctionByName(functionName, context /*, args */) {
+	var args = Array.prototype.slice.call(arguments).splice(2);
+	var namespaces = functionName.split(".");
+	var func = namespaces.pop();
+	for ( var i = 0; i < namespaces.length; i++ ) {
+		context = context[namespaces[i]];
+	}
+	return context[func].apply( this, args );
+}
+
 // Speed up calls to hasOwnProperty
 var hasOwnProperty = Object.prototype.hasOwnProperty;
-/*
+/**
  * Checks whether parameter is empty
  * True for '', [], {}
  * Custom check: is_empty({length: 0, custom_property: []})
  * @uses hasOwnProperty
+ * @param param mixed Can be anything.
  */
 function tsIsEmpty( param ) {
 	"use strict";
@@ -56,7 +104,46 @@ function tsIsEmpty( param ) {
     return true;
 }
 
+/**
+ * Merges default and function args over-writing defaults.
+ *
+ * @param {array}  args     Array of args.
+ * @param {array}  defaults Array of default args.
+ * @return {array} args     Merged array of args.
+ */
+function tsMergeArgs( args, defaults ) {
+	"use strict";
+	
+    for ( var i in defaults ) {
+    	if ( "undefined" == typeof args[i] ) {
+			args[index] = defaults[i];
+		}
+   }
+   
+   return args;
+}
+
+/**
+ * Prints Current Slide
+ */
+function tsPrint() {
+	window.print();
+}
+
+
+/*****
+STUDENT FUNCTIONS
+*****/
+
 // Creates Student Object
+var student = {};
+/**
+ * Initializes Student Object
+ *
+ * @uses tsIsEmpty() 
+ * @uses tsAPI
+ * @return {object} student Student Object
+ */
 function tsGetStudentData() {
 	"use strict";
 	
@@ -74,20 +161,42 @@ function tsGetStudentData() {
 	return student;
 }
 
-/*
+/**
  * Checks if student has property & adds if not
  * Does not over-ride current property
+ *
+ * @param {string} property      Property to be added to student object.
+ * @param {string} articulateVar Pulls Variable Value from Articulate.
  */
 function tsStudentAddProperty( property, articulateVar ){
+	"use strict";
+	
 	if ( ! student.hasOwnProperty( property ) ) { 
 		student[property] = tsPlayer.GetVar( articulateVar );
 	}
 }
 
-/*
+/**
  * Over-rides current property & updates
+ *
+ * @param {string} property      Property to be added to student object.
+ * @param {string} articulateVar Pulls Variable Value from Articulate.
  */
 function tsStudentUpdateProperty( property, articulateVar ){
+	"use strict";
+	
 	student[property] = tsPlayer.GetVar( articulateVar );
+}
+
+/**
+ * Over-rides current property & updates
+ *
+ * @param {string} property      Property to be added to student object.
+ * @param {string} articulateVar Pulls Variable Value from Articulate.
+ */
+function tsStudentDeleteProperty( property ){
+	"use strict";
+	
+	delete student[property];
 }
 
